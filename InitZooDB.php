0b08ff47b5f54f24749dca847ee143cf1459f3fd
bendelv <ben.delvoye@gmail.com>
2018-04-05 21:23:12 +0200

    Template to create tables

--- InitZooDB.php
+++ InitZooDB.php
@@ -35,15 +35,59 @@ try{
   echo "Connected to DB \"$dbName\".. <br>";
 
   //Creation of tables
+  $sql = "CREATE TABLE IF NOT EXISTS Institution(
+  nom_institution VARCHAR(50),
+  rue VARCHAR(50),
+  code_postal int,
+  pays VARCHAR(50),
+  PRIMARY KEY(nom_institution)
+  )";
+  $BDD->exec($sql);
+  echo "Table \" institution\" created successfully";
+
+  $sql = "CREATE TABLE IF NOT EXISTS Espece(
+  nom_scientifique VARCHAR(50),
+  nom_courant VARCHAR(50),
+  regime_alimentaire VARCHAR(50),
+  PRIMARY KEY(nom_scientifique)
+  )";
+  $BDD->exec($sql);
+  echo "Table \"Espece\" created successfully";
+
   $sql = "CREATE TABLE IF NOT EXISTS Climat(
   nom_scientifique VARCHAR(50),
   nom_climat VARCHAR(50),
   PRIMARY KEY(nom_climat, nom_scientifique),
-
+  FOREIGN KEY(nom_scientifique) REFERENCES (Espece)
   )";
-
   $BDD->exec($sql);
-  echo "Table Climat created successfully";
+  echo "Table \"Climat\" created successfully";
+
+  $sql = "CREATE TABLE IF NOT EXISTS Animal(
+  nom_scientifique
+  PRIMARY KEY(),
+  FOREIGN KEY() REFERENCES ()
+  )";
+  $BDD->exec($sql);
+  echo "Table  created successfully";
+
+  $sql = "CREATE TABLE IF NOT EXISTS (
+
+  PRIMARY KEY(),
+  FOREIGN KEY() REFERENCES ()
+  )";
+  $BDD->exec($sql);
+  echo "Table created successfully";
+
+  $sql = "CREATE TABLE IF NOT EXISTS (
+
+  PRIMARY KEY(),
+  FOREIGN KEY() REFERENCES ()
+  )";
+  $BDD->exec($sql);
+  echo "Table created successfully";
+
+
 
 }catch(PDOException $e){
   echo $sql . "<br>" . $e->getMessage();
